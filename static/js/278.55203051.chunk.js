6c79fa5b510c7f7d7183c545784491ede6601cd9
Shako1510 <93245492+Shako1510@users.noreply.github.com>
2023-01-26 13:25:18 +0000

    Deploying to gh-pages from @ Shako1510/goit-react-hw-05-movies@018c6033c0ffa425661946bc8a1b5e818e27da9b 🚀

---
+++ static/js/278.55203051.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[278],{278:function(e,t,r){r.r(t);r(791);var n=r(184);t.default=function(){return(0,n.jsx)("main",{children:(0,n.jsx)("h1",{children:" Movies "})})}}}]);
+//# sourceMappingURL=278.55203051.chunk.js.map
---
+++ static/js/544.0d904b26.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{544:function(e,t,r){r.r(t);r(791);var a=r(184);t.default=function(){return(0,a.jsx)("main",{children:(0,a.jsx)("h1",{children:" Home Page"})})}}}]);
+//# sourceMappingURL=544.0d904b26.chunk.js.map
